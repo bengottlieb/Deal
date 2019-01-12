e84ff8214f453dfbe69e97fe814a817647d86519
Ben Gottlieb <ben@standalone.com>
2019-01-12 05:54:08 -0600

    initial commit

---
+++ TestHarness_Mac/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  TestHarness_Mac
+//
+//  Created by Ben Gottlieb on 12/21/18.
+//  Copyright © 2018 Stand Alone, inc. All rights reserved.
+//
+
+import Cocoa
+import Deal
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
